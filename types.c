645bb4f7e692f567660f20e6a64a478cd1452b96
H.J. Lu <hjl.tools@gmail.com>
2012-04-25 14:25:30 -0700

    Add check for <sys/statfs.h>

--- types.c
+++ types.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #ifdef KERNEL_HDR
 #include <asm/siginfo.h>
+#include <asm/statfs.h>
 #include <linux/ipc.h>
 #include <linux/mqueue.h>
 #include <linux/resource.h>
@@ -10,6 +11,7 @@
 #include <time.h>
 #include <sys/ipc.h>
 #include <sys/resource.h>
+#include <sys/statfs.h>
 #endif
 
 void
@@ -38,5 +40,7 @@ main ()
 	 __builtin_offsetof (siginfo_t, si_utime));
   value ("size of struct mq_attr", sizeof (struct mq_attr));
   value ("size of struct rlimit", sizeof (struct rlimit));
+  value ("size of struct statfs", sizeof (struct statfs));
+  value ("size of struct statfs64", sizeof (struct statfs64));
   return 0;
 }
