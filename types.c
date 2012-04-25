a6e8d66faf3f0899020a038644ac3bb19bd78e11
H.J. Lu <hjl.tools@gmail.com>
2012-04-25 15:53:20 -0700

    Add <sys/types.h> test

--- types.c
+++ types.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #ifdef KERNEL_HDR
+#include <asm/posix_types.h>
 #include <asm/siginfo.h>
 #include <asm/statfs.h>
 #include <linux/ipc.h>
@@ -14,6 +15,7 @@
 #include <sys/resource.h>
 #include <sys/statfs.h>
 #include <sys/timex.h>
+#include <sys/types.h>
 #endif
 
 void
@@ -26,12 +28,26 @@ int
 main ()
 {
 #ifdef KERNEL_HDR
+  value ("size of ino_t", sizeof (__kernel_ino_t));
+  value ("size of pid_t", sizeof (__kernel_pid_t));
+  value ("size of uid_t", sizeof (__kernel_uid32_t));
+  value ("size of gid_t", sizeof (__kernel_gid32_t));
+  value ("size of fsid_t", sizeof (__kernel_fsid_t));
   value ("size of clock_t", sizeof (__kernel_clock_t));
+  value ("size of suseconds_t", sizeof (__kernel_suseconds_t));
+  value ("size of daddr_t", sizeof (__kernel_daddr_t));
   value ("size of __ipc_pid_t", sizeof (__kernel_ipc_pid_t));
   value ("size of struct ipc_perm", sizeof (struct ipc64_perm));
   value ("size of mqd_t", sizeof (__kernel_mqd_t));
 #else
+  value ("size of ino_t", sizeof (ino_t));
+  value ("size of pid_t", sizeof (pid_t));
+  value ("size of uid_t", sizeof (uid_t));
+  value ("size of gid_t", sizeof (gid_t));
+  value ("size of fsid_t", sizeof (fsid_t));
   value ("size of clock_t", sizeof (clock_t));
+  value ("size of suseconds_t", sizeof (suseconds_t));
+  value ("size of daddr_t", sizeof (daddr_t));
   value ("size of __ipc_pid_t", sizeof (__ipc_pid_t));
   value ("size of struct ipc_perm", sizeof (struct ipc_perm));
   value ("size of mqd_t", sizeof (mqd_t));
